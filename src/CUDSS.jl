b12d501e9a553543b381298f516b6e35f9b3bd08
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-12-11 23:11:03 -0600

    Add a method \ for CudssSolver

--- src/CUDSS.jl
+++ src/CUDSS.jl
@@ -7,6 +7,7 @@ using SparseArrays
 
 import CUDA: @checked, libraryPropertyType, cudaDataType, initialize_context, retry_reclaim, CUstream
 import LinearAlgebra: lu, lu!, ldlt, ldlt!, cholesky, cholesky!, ldiv!, BlasFloat, BlasReal
+import Base: \
 
 include("libcudss.jl")
 include("error.jl")
