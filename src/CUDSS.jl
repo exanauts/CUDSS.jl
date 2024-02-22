0aea6535eebe6e4478b58fe90eac862de7a7ad77
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-02-22 22:35:55 +0100

    Add an option to use cuDSS without an artifact

--- src/CUDSS.jl
+++ src/CUDSS.jl
@@ -5,6 +5,14 @@ using CUDSS_jll
 using LinearAlgebra
 using SparseArrays
 
+if haskey(ENV, "JULIA_CUDSS_LIBRARY_PATH") && Sys.islinux()
+  const libcudss = joinpath(ENV["JULIA_CUDSS_LIBRARY_PATH"], "libcudss.so")
+  const CUDSS_INSTALLATION = "CUSTOM"
+else
+  using CUDSS_jll
+  const CUDSS_INSTALLATION = "YGGDRASIL"
+end
+
 import CUDA: @checked, libraryPropertyType, cudaDataType, initialize_context, retry_reclaim, CUstream
 import LinearAlgebra: lu, lu!, ldlt, ldlt!, cholesky, cholesky!, ldiv!, BlasFloat, BlasReal
 import Base: \
