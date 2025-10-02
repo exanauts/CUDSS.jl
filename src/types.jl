21d65c4d49a5669a468b3f714f2bf1a2f26c603d
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-10-02 12:04:45 -0500

    Interface the new features of cuDSS 0.7

--- src/types.jl
+++ src/types.jl
@@ -3,14 +3,17 @@
 const CUDSS_DATA_PARAMETERS = ("info", "lu_nnz", "npivots", "inertia", "perm_reorder_row",
                                "perm_reorder_col", "perm_row", "perm_col", "diag", "user_perm",
                                "hybrid_device_memory_min", "comm", "memory_estimates",
-                               "perm_matching", "scale_row", "scale_col")
+                               "perm_matching", "scale_row", "scale_col", "nsuperpanels",
+                               "user_schur_indices", "schur_shape", "schur_matrix",
+                               "user_elimination_tree", "elimination_tree", "user_host_interrupt")
 
 const CUDSS_CONFIG_PARAMETERS = ("reordering_alg", "factorization_alg", "solve_alg", "use_matching",
                                  "matching_alg", "solve_mode", "ir_n_steps", "ir_tol", "pivot_type",
                                  "pivot_threshold", "pivot_epsilon", "max_lu_nnz", "hybrid_mode",
                                  "hybrid_device_memory_limit", "use_cuda_register_memory", "host_nthreads",
                                  "hybrid_execute_mode", "pivot_epsilon_alg", "nd_nlevels", "ubatch_size",
-                                 "ubatch_index")
+                                 "ubatch_index", "use_superpanels", "device_count", "device_indices",
+                                 "schur_mode" "deterministic_mode")
 
 const CUDSS_TYPES = Dict{String, Type}(
     # data type
@@ -30,6 +33,13 @@ const CUDSS_TYPES = Dict{String, Type}(
     "perm_matching" => Vector{Cint},
     "scale_row" => Vector{Float64},
     "scale_col" => Vector{Float64},
+    "nsuperpanels" => Cint,
+    "user_schur_indices" => Cint,
+    "schur_shape" => Tuple{Int64, Int64, Int64},
+    "schur_matrix" => cudssMatrix_t
+    "user_elimination_tree" => Vector{Cint},
+    "elimination_tree" => Vector{Cint},
+    "user_host_interrupt" => Vector{Cint},
     # config type
     "reordering_alg" => cudssAlgType_t,
     "factorization_alg" => cudssAlgType_t,
@@ -52,6 +62,11 @@ const CUDSS_TYPES = Dict{String, Type}(
     "nd_nlevels" => Cint,
     "ubatch_size" => Cint,
     "ubatch_index" => Cint,
+    "use_superpanels" => Cint,
+    "device_count" => Cint,
+    "device_indices" => Vector{Cint},
+    "schur_mode" => Cint,
+    "deterministic_mode" => Cint,
 )
 
 ## config type
@@ -99,6 +114,16 @@ function Base.convert(::Type{cudssConfigParam_t}, config::String)
         return CUDSS_CONFIG_UBATCH_SIZE
     elseif config == "ubatch_index"
         return CUDSS_CONFIG_UBATCH_INDEX
+    elseif config == "use_superpanels"
+        return CUDSS_CONFIG_USE_SUPERPANELS
+    elseif config == "device_count"
+        return CUDSS_CONFIG_DEVICE_COUNT
+    elseif config == "device_indices"
+        return CUDSS_CONFIG_DEVICE_INDICES
+    elseif config == "schur_mode"
+        return CUDSS_CONFIG_SCHUR_MODE
+    elseif config == "deterministic_mode"
+        return CUDSS_CONFIG_DETERMINISTIC_MODE
     else
         throw(ArgumentError("Unknown config parameter $config"))
     end
@@ -139,6 +164,20 @@ function Base.convert(::Type{cudssDataParam_t}, data::String)
         return CUDSS_DATA_SCALE_ROW
     elseif data == "scale_col"
         return CUDSS_DATA_SCALE_COL
+    elseif data == "nsuperpanels"
+        return CUDSS_DATA_NSUPERPANELS
+    elseif data == "user_schur_indices"
+        return CUDSS_DATA_USER_SCHUR_INDICES
+    elseif data == "schur_shape"
+        return CUDSS_DATA_SCHUR_SHAPE
+    elseif data == "schur_matrix"
+        return CUDSS_DATA_SCHUR_MATRIX
+    elseif data == "user_elimination_tree"
+        return CUDSS_DATA_USER_ELIMINATION_TREE
+    elseif data == "elimination_tree"
+        return CUDSS_DATA_ELIMINATION_TREE
+    elseif data == "user_host_interrupt"
+        return CUDSS_DATA_USER_HOST_INTERRUPT
     else
         throw(ArgumentError("Unknown data parameter $data"))
     end
@@ -157,12 +196,18 @@ function Base.convert(::Type{cudssPhase_t}, phase::String)
         return CUDSS_PHASE_FACTORIZATION
     elseif phase == "refactorization"
         return CUDSS_PHASE_REFACTORIZATION
+    elseif phase == "solve_fwd_perm"
+        return CUDSS_PHASE_SOLVE_FWD_PERM
     elseif phase == "solve_fwd"
         return CUDSS_PHASE_SOLVE_FWD
     elseif phase == "solve_diag"
         return CUDSS_PHASE_SOLVE_DIAG
     elseif phase == "solve_bwd"
         return CUDSS_PHASE_SOLVE_BWD
+    elseif phase == "solve_bwd_perm"
+        return CUDSS_PHASE_SOLVE_BWD_PERM
+    elseif phase == "solve_refinement"
+        return CUDSS_PHASE_SOLVE_REFINEMENT
     elseif phase == "solve"
         return CUDSS_PHASE_SOLVE
     else
@@ -220,9 +265,9 @@ end
 
 function Base.convert(::Type{cudssLayout_t}, layout::Char)
     if layout == 'R'
-        CUDSS_LAYOUT_ROW_MAJOR
+        return CUDSS_LAYOUT_ROW_MAJOR
     elseif layout == 'C'
-        CUDSS_LAYOUT_COL_MAJOR
+        return CUDSS_LAYOUT_COL_MAJOR
     else
         throw(ArgumentError("Unknown layout $layout"))
     end
@@ -232,17 +277,17 @@ end
 
 function Base.convert(::Type{cudssAlgType_t}, algorithm::String)
     if algorithm == "default"
-        CUDSS_ALG_DEFAULT
+        return CUDSS_ALG_DEFAULT
     elseif algorithm == "algo1"
-        CUDSS_ALG_1
+        return CUDSS_ALG_1
     elseif algorithm == "algo2"
-        CUDSS_ALG_2
+        return CUDSS_ALG_2
     elseif algorithm == "algo3"
-        CUDSS_ALG_3
+        return CUDSS_ALG_3
     elseif algorithm == "algo4"
-        CUDSS_ALG_4
+        return CUDSS_ALG_4
     elseif algorithm == "algo5"
-        CUDSS_ALG_5
+        return CUDSS_ALG_5
     else
         throw(ArgumentError("Unknown algorithm $algorithm"))
     end
