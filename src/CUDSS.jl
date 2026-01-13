d26c3d3b2eed91b0eada7742b4a17ccbe68deac6
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2026-01-13 11:48:47 -0600

    Detect local installation of cuDSS (#57)

--- src/CUDSS.jl
+++ src/CUDSS.jl
@@ -6,12 +6,37 @@ using CUDSS_jll
 using LinearAlgebra
 using SparseArrays
 
-if haskey(ENV, "JULIA_CUDSS_LIBRARY_PATH") && Sys.islinux()
-  const libcudss = joinpath(ENV["JULIA_CUDSS_LIBRARY_PATH"], "libcudss.so")
-  const CUDSS_INSTALLATION = "CUSTOM"
+if CUDA.local_toolkit
+    using CUDA_Runtime_Discovery
 else
-  using CUDSS_jll
-  const CUDSS_INSTALLATION = "YGGDRASIL"
+    import CUDSS_jll
+end
+
+function __init__()
+  if CUDA.functional()
+    global libcudss
+    global CUDSS_INSTALLATION
+    if haskey(ENV, "JULIA_CUDSS_LIBRARY_PATH") && Sys.islinux()
+      libcudss = joinpath(ENV["JULIA_CUDSS_LIBRARY_PATH"], "libcudss.so")
+      CUDSS_INSTALLATION = "CUSTOM"
+    elseif CUDA.local_toolkit
+      dirs = CUDA_Runtime_Discovery.find_toolkit()
+      path = CUDA_Runtime_Discovery.get_library(dirs, "cudss"; optional=true)
+      (path === nothing) && error("cuDSS is not available on your system (looked in $(join(dirs, ", "))).")
+      libcudss = path
+      cudss_version = version()
+      if v"0.7.0" <= cudss_version < v"0.8"
+        CUDSS_INSTALLATION = "LOCAL"
+      else
+        libcudss = nothing # Reset the library to nothing because we are failing out (just in case)
+        error("cuDSS found: $(path) is version $(cudss_version) but CUDSS.jl expects version 0.7.*")
+      end
+    else
+      !CUDSS_jll.is_available() && error("cuDSS is not available for your platform.")
+      libcudss = CUDSS_jll.libcudss
+      CUDSS_INSTALLATION = "YGGDRASIL"
+    end
+  end
 end
 
 import CUDA: libraryPropertyType, cudaDataType, initialize_context, retry_reclaim, CUstream, unsafe_free!
