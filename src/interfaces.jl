d1138506ce872f792c5f0c1d041d024e1cb4cfe0
Michel Schanen <michel.schanen@gmail.com>
2026-05-11 09:46:56 -0500

    compat with CUDA@6.0.0 (#144)
    * compat with CUDA@6.0.0
    
    * Move CI to kkt and add CUDA 13
    
    * Bump to v0.7.0
    
    * Update CI flow
    
    * Bump CUDA_Runtime_Discovery
    
    ---------
    
    Co-authored-by: Hans Würfel <git@wuerfel.io>

--- src/interfaces.jl
+++ src/interfaces.jl
@@ -526,7 +526,7 @@ function cudss(phase::String, solver::CudssSolver{T}, X::CudssMatrix{T}, B::Cuds
   (phase == "refactorization") && cudss_set(solver, "info", 0)
   cudss_phase = convert(cudssPhase_t, phase)
   cudssExecute(solver.data.handle, cudss_phase, solver.config, solver.data, solver.matrix, X, B)
-  !asynchronous && CUDA.synchronize()
+  !asynchronous && CUDACore.synchronize()
   if (phase == "factorization") && solver.fresh_factorization
     solver.fresh_factorization = false
   end
@@ -551,7 +551,7 @@ function cudss(phase::String, solver::CudssBatchedSolver{T}, X::CudssBatchedMatr
   (phase == "refactorization") && cudss_set(solver, "info", 0)
   cudss_phase = convert(cudssPhase_t, phase)
   cudssExecute(solver.data.handle, cudss_phase, solver.config, solver.data, solver.matrix, X, B)
-  !asynchronous && CUDA.synchronize()
+  !asynchronous && CUDACore.synchronize()
   if (phase == "factorization") && solver.fresh_factorization
     solver.fresh_factorization = false
   end
